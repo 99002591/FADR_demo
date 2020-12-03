19300608c391c1570789bbe05b48ae45cf8a0e9d
99002591 <amit.das@ltts.com>
2020-12-03 18:32:57 +0530

    all loginAPI tests online

---
+++ src/test/java/REST/pojo.java
@@ -0,0 +1,24 @@
+package REST;
+
+public class pojo {
+
+	private String password;
+	private String email;
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return this.password;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return this.email;
+	}
+
+}
---
+++ src/test/java/REST/readJSON.java
@@ -0,0 +1,21 @@
+package REST;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class readJSON {
+	
+	public String json_content = null,line;
+	
+	public String readJSONfile(String string) throws Exception {
+		FileReader f_read = new FileReader("D:/Eclipse_Workspace/fadrdemo/src/test/resources/API_requests/"+string);
+		BufferedReader b_read = new BufferedReader(f_read);
+		 while ((line = b_read.readLine()) != null) {
+			 json_content += line;
+		 }
+		 b_read.close();
+		 f_read.close();
+		 json_content = json_content.replaceAll("null", "");
+		return json_content;
+	}
+}
